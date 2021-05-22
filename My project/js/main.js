6f80fde0924e5f07870076fccd8ed0b38e0006e7
Gennady <79419568+GennadyYanushevsky@users.noreply.github.com>
2021-05-22 19:37:36 +0300

    Add files via upload

---
+++ My project/js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+$('.slider__inner, .description__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn  slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn  slick-prev"></button>',
+    infinite:false,
+    
+    
+    
+
+});
+
+$('.news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn  slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn  slick-prev"></button>',
+    infinite:true,
+    autoplay:true,
+    autoplaySpeed: 5000,
+    speed:500
+
+});
+
+
+});
